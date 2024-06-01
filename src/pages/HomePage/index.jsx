f2f17aef47420272d641dee2d0a9bbdd45f9bf0b
Iryna Kakalets <irinakakalec@gmail.com>
2024-06-01 17:44:08 +0200

    Úkol č. 16 -  Pokračování vtipu

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,47 +1,35 @@
-import './style.css';
-import { useState } from 'react';
+import { useState, useEffect } from "react";
+import { Joke } from "../../components/Joke";
 
 export const HomePage = () => {
-  const [likesCount, setLikesCount] = useState(0);
-  const [dislikesCount, setDislikesCount] = useState(0);
+  const [jokes, setJokes] = useState([]);
 
-  const avatar =
-    'https://raw.githubusercontent.com/Czechitas-podklady-WEB/dadjokes/main/users/user01.png';
+  useEffect(() => {
+    const fetchJokes = async () => {
+      const response = await fetch(
+        "http://localhost:4000/api/jokes"
+      );
+      const json = await response.json();
+      const data = json.data;
+      setJokes(data);
+    };
+    fetchJokes();
+  }, []);
 
   return (
     <div className="container">
-      <div className="joke">
-        <div className="joke__body">
-          <div className="joke__user">
-            <img className="user-avatar" src={avatar} />
-            <p className="user-name">Neroxx</p>
-          </div>
-
-          <p className="joke__text">
-            The secret service isn&apos;t allowed to yell &quot;Get down!&quot;
-            anymore when the president is about to be attacked. Now they have to
-            yell &quot;Donald, duck!&quot;
-          </p>
-        </div>
-        <div className="joke__likes">
-          <button
-            id="btn-up"
-            className="btn-like btn-like--up"
-            onClick={() => setLikesCount(likesCount + 1)}
-          ></button>
-          <span id="likes-up" className="likes-count likes-count--up">
-            {likesCount}
-          </span>
-          <button
-            id="btn-down"
-            className="btn-like btn-like--down"
-            onClick={() => setDislikesCount(dislikesCount + 1)}
-          ></button>
-          <span id="likes-down" className="likes-count likes-count--down">
-            {dislikesCount}
-          </span>
-        </div>
-      </div>
+      {jokes.map((joke) => {
+        return (
+          <Joke
+            key={joke.id}
+            userAvatar={joke.avatar}
+            userName={joke.name}
+            text={joke.text}
+            likes={joke.likes}
+            dislikes={joke.dislikes}
+          />
+        );
+      })}
     </div>
   );
 };
